c1aefce92781f510093de3d16fe0739dbfdc0f79
Yanze LIU <yanzeliu.france@gmail.com>
2016-07-27 18:13:42 -0700

    Initial Commit

---
+++ HypnosisView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HypnosisView
+//
+//  Created by yanze on 7/27/16.
+//  Copyright © 2016 yanzeliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HypnosisView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HypnosisView
+//
+//  Created by yanze on 7/27/16.
+//  Copyright © 2016 yanzeliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
